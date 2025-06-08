91a78cf467285472c3664854b9e162ae9470a3d8
Flopster101 <nahuelgomez329@gmail.com>
2025-06-08 20:39:38 -0300

    init: Implement a way to select IR blaster device driver
    Now that official LOS for ginkgo/laurel_sprout uses the LIRC-based Lineage
    IR HAL, I need to implement a way to support this and the old spidev-based
    HAL.
    
    This should work, but ideally spidev.c should be cleaned up.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- include/linux/workarounds.h
+++ include/linux/workarounds.h
@@ -1,3 +1,4 @@
 /* SPDX-License-Identifier: GPL-2.0 */
- 
+
 bool is_legacy_timestamp(void);
+bool is_using_legacy_ir_hal(void);
--- init/main.c
+++ init/main.c
@@ -138,7 +138,27 @@ static char *ramdisk_execute_command;
 
 /* Workarounds */
 static bool legacy_timestamp_source = false;
- 
+
+/* Workaround: legacy_ir_hal */
+static bool legacy_ir_hal = false;
+
+static int __init set_legacy_ir_hal(char *val)
+{
+	int tmp = legacy_ir_hal;
+
+	if (get_option(&val, &tmp)) {
+		legacy_ir_hal = tmp != 0;
+	}
+
+	return 0;
+}
+__setup("legacy_ir_hal=", set_legacy_ir_hal);
+
+bool is_using_legacy_ir_hal(void)
+{
+	return legacy_ir_hal;
+}
+
 static int __init set_timestamp_source(char *val)
 {
 	int tmp = legacy_timestamp_source;
