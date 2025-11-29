9afc8c578d059cbaae79b2c30bf19e2fd202f067
TheSillyOk <priv.ld@proton.me>
2025-11-29 14:56:03 -0300

    treewide: Implememt SUSFS v2.0.0 for non-GKI
    Apply patches from TheSillyOk/kernel_ls_patches.[1]
    
    [1] -
    https://github.com/TheSillyOk/kernel_ls_patches/commit/cf4ef378c6c9d0bcaf1e2c59d6c83faca4050e76
    Signed-off-by: TheSillyOk <priv.ld@proton.me>
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -63,6 +63,9 @@
 #include <linux/compat.h>
 #include <linux/vmalloc.h>
 #include <linux/task_integrity.h>
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs_def.h>
+#endif
 
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
@@ -1867,6 +1870,14 @@ static int __do_execve_file(int fd, struct filename *filename,
 			    struct user_arg_ptr argv,
 			    struct user_arg_ptr envp,
 			    int flags, struct file *file)
+#ifdef CONFIG_KSU_SUSFS
+extern bool ksu_execveat_hook __read_mostly;
+extern bool __ksu_is_allow_uid_for_current(uid_t uid);
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv,
+				void *envp, int *flags);
+#endif
 {
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
@@ -1875,6 +1886,17 @@ static int __do_execve_file(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
+#ifdef CONFIG_KSU_SUSFS
+	if (likely(susfs_is_current_proc_umounted())) {
+		goto orig_flow;
+	}
+	if (unlikely(ksu_execveat_hook)) {
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	} else if ((__ksu_is_allow_uid_for_current(current_uid().val))) {
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	}
+orig_flow:
+#endif
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
