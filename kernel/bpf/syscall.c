81c0074036f3f580fc5f21fddbda5f613305ab4f
Vayruz Rafli <mikhailovnakowalski@gmail.com>
2025-12-07 15:09:30 +0000

    Revert "bpf: offload: free program id when device disappears"
    This reverts commit 3ce5151ff969db3446ca854860055c2c74efa99d.
    
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -968,13 +968,9 @@ static int bpf_prog_alloc_id(struct bpf_prog *prog)
 	return id > 0 ? 0 : id;
 }
 
-void bpf_prog_free_id(struct bpf_prog *prog, bool do_idr_lock)
+static void bpf_prog_free_id(struct bpf_prog *prog, bool do_idr_lock)
 {
-	/* cBPF to eBPF migrations are currently not in the idr store.
-	 * Offloaded programs are removed from the store when their device
-	 * disappears - even if someone grabs an fd to them they are unusable,
-	 * simply waiting for refcnt to drop to be freed.
-	 */
+	/* cBPF to eBPF migrations are currently not in the idr store. */
 	if (!prog->aux->id)
 		return;
 
@@ -985,7 +981,6 @@ void bpf_prog_free_id(struct bpf_prog *prog, bool do_idr_lock)
 
 	idr_remove(&prog_idr, prog->aux->id);
 
-	prog->aux->id = 0;
 	if (do_idr_lock)
 		spin_unlock_bh(&prog_idr_lock);
 	else
